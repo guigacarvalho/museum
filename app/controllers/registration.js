5ce13d0f09d79c162f5cb6c9fe90ac89097bfa2e
SamyukthaJ <samyukthaj08@gmail.com>
2014-08-17 00:04:51 -0700

    Registration model with newsletter and gender

--- app/controllers/registration.js
+++ app/controllers/registration.js
@@ -1,27 +1,40 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	isNotValid: false,
 	actions:{
 		save: function () {
-			var visitor = this.store.createRecord('registration',
-				{
-					zip: this.get('zip'),
-					email: this.get('email'),
-					dob: this.get('dob')
-				}
-			);
-			var controller = this;
-			visitor.save().then(
-				function() {
-					console.log("saved!!");
-					controller.get('model.registration');
-					controller.set('email', '');
-					controller.set('zip', '');
-					controller.set('dob', '');					
+			var email = this.get('email'),
+				zip = this.get('zip'),
+				day = this.get('day'),
+				month = this.get('month'),
+				year = this.get('year'),
+				mailing_list = this.get('mailing_list');
+			if ( zip && day && month && year ) {
+				this.set('isNotValid' , false);
+				console.log('inside if');
+				var visitor = this.store.createRecord('registration', {
+					zip: zip,
+					email: email,
+					dob: day + '/' + month + '/' + year,
+					mailing_list: mailing_list
+				});
+				var controller = this;
+				visitor.save().then(
+					function() {
+						console.log("saved!!");
+						controller.get('model.registration');
+						controller.set('email', '');
+						controller.set('zip', '');
+						controller.set('day', '');
+						controller.set('month', '');
+						controller.set('year', '');						
 					// controller.addObject(visitor);
-				}
-			);
-
+					}
+				);
+			} else {
+				this.set('isNotValid' , true);
+			}
 		}
 	}
 });
--- app/models/registration.js
+++ app/models/registration.js
@@ -3,7 +3,8 @@ import DS from 'ember-data';
 export default DS.Model.extend({
     email: DS.attr('string'),
     zip: DS.attr('string'),
-    dob: DS.attr('string')
+    dob: DS.attr('string'),
+    mailing_list: DS.attr('boolean')
     // gender: DS.attr('string'),
     // newsletter: DS.attr('boolean')
 });
