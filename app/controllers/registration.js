ea4489617e0cd5e9dd0a66369a92f50b1d0ea1c3
SamyukthaJ <samyukthaj08@gmail.com>
2014-08-18 11:09:36 -0700

    Implemented login/logout functionality.
    The form is hidden and welcome message is displayed. Validation on zip
    code.

--- app/controllers/registration.js
+++ app/controllers/registration.js
@@ -1,7 +1,24 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+var RegistrationController = Ember.ArrayController.extend(Ember.Validations.Mixin , {
+	gender: ["Gender","male","female"],
+	selectedGender: "Gender",
+	newsletter: true,
+	needs: ['application'],
+	loggedout: Ember.computed.alias('controllers.application.is_loggedout'),
 	isNotValid: false,
+	validations: {
+    	zip: {
+    		presence: true,
+    		length: { minimum: 5 },
+    		numericality: true
+    	}
+	},
+	watchChanges: function () {
+		
+		this.validate();
+		console.log("in validate");
+	}.observes("zip"),
 	actions:{
 		save: function () {
 			var email = this.get('email'),
@@ -9,15 +26,24 @@ export default Ember.Controller.extend({
 				day = this.get('day'),
 				month = this.get('month'),
 				year = this.get('year'),
-				mailing_list = this.get('mailing_list');
+				gender = this.get('selectedGender'),
+				newsletter = this.get('newsletter');
+
+			if(zip.match("^\\d{5}$")){
+				console.log('match found');
+			} else {
+				console.log('match not found');
+			}
 			if ( zip && day && month && year ) {
 				this.set('isNotValid' , false);
-				console.log('inside if');
+				this.set('loggedout', false);
+				console.log(this.get('selectedGender') + newsletter);
 				var visitor = this.store.createRecord('registration', {
 					zip: zip,
 					email: email,
 					dob: day + '/' + month + '/' + year,
-					mailing_list: mailing_list
+					gender: gender,
+					newsletter: newsletter
 				});
 				var controller = this;
 				visitor.save().then(
@@ -28,13 +54,25 @@ export default Ember.Controller.extend({
 						controller.set('zip', '');
 						controller.set('day', '');
 						controller.set('month', '');
-						controller.set('year', '');						
+						controller.set('year', '');
+						controller.set('gender', '');
+						controller.set('newsletter', '');						
 					// controller.addObject(visitor);
 					}
 				);
 			} else {
+				this.set('loggedout', true);
 				this.set('isNotValid' , true);
 			}
+		},
+		logout: function () {
+			this.set('loggedout', true);
+			this.set('isNotValid' , false);
+			this.set('selectedGender','Gender');
+			this.set('newsletter', true);
 		}
 	}
 });
+
+
+export default RegistrationController;
--- app/models/registration.js
+++ app/models/registration.js
@@ -4,7 +4,6 @@ export default DS.Model.extend({
     email: DS.attr('string'),
     zip: DS.attr('string'),
     dob: DS.attr('string'),
-    mailing_list: DS.attr('boolean')
-    // gender: DS.attr('string'),
-    // newsletter: DS.attr('boolean')
+    gender: DS.attr('string'),
+    newsletter: DS.attr('boolean')
 });
