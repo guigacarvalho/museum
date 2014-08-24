c9f65c54c38361d56511f4c88794c713afdeea46
guigacarvalho <gcarvalho@scu.edu>
2014-08-24 09:59:42 -0700

    Adding titles from open street map and pins

--- app/controllers/map.js
+++ app/controllers/map.js
@@ -1,8 +1,13 @@
 import Ember from 'ember';
 
+
 export default Ember.ArrayController.extend({
-	content: [
-      {location: L.latLng(40.714, -74.000)},
-      {location: L.latLng(40.714, -73.989)},
-      {location: L.latLng(40.721, -73.991)}]
-});
+  content: [{
+      location: L.latLng(37.414287, -122.077409),
+      title: 'Computer History Museum'
+		},
+		{
+      location: L.latLng(37.414387, -122.077509),
+      title: 'Test Gallery 1'
+		}]
+  });
--- app/views/map.js
+++ app/views/map.js
@@ -1,7 +1,18 @@
-// import Ember from 'ember';
-
 export default EmberLeaflet.MapView.extend({
-  childLayers: [
-      EmberLeaflet.DefaultTileLayer,
-      MyNewAppENV.MarkerCollectionLayer]
-});
+	 	childLayers: [
+		  EmberLeaflet.TileLayer.extend({
+		  	tileUrl: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+			options: {
+						attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'}}),
+		  EmberLeaflet.MarkerCollectionLayer.extend({
+		  	contentBinding: 'controller',
+		  	itemLayerClass: EmberLeaflet.MarkerLayer.extend(
+		  		EmberLeaflet.PopupMixin, {
+					  popupContentBinding: 'content.title'
+					}
+			    )
+			  })
+		],
+		  center: L.latLng(37.414287, -122.077409),
+		  zoom:18
+  });
