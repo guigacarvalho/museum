c49bb123a82d042503bf0cdd8d45894f94d1de4d
guigacarvalho <gcarvalho@scu.edu>
2014-08-25 17:56:00 -0700

    Updating the adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.FirebaseAdapter.extend({
-	firebase: new Firebase("popping-fire-5325.firebaseio.com")
+	firebase: new Firebase("https://popping-fire-5325.firebaseio.com/")
 });
